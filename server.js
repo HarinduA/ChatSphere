d4e34393309448ed4f64e0dc338092c6f2a76061
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-07-27 22:38:42 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import cors from 'cors';
+import express from 'express';
+import { readFile } from 'node:fs/promises';
+import { authMiddleware, handleLogin } from './auth.js';
+import { resolvers } from './resolvers.js';
+import { WebSocketServer } from 'ws';
+import { createServer as createHttpServer } from 'node:http';
+import { useServer as useWsServer } from 'graphql-ws/lib/use/ws'
+
+
+const PORT = 9000;
+
+const app = express();
+app.use(cors(), express.json());
+
+app.post('/login', handleLogin);
+
+function getContext({ req }) {
+  if (req.auth) {
+    return { user: req.auth.sub };
+  }
+  return {};
+}
+
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+const schema = makeExecutableSchema ({ typeDefs, resolvers});
+
+const apolloServer = new ApolloServer({ schema });
+await apolloServer.start();
+app.use('/graphql', authMiddleware, apolloMiddleware(apolloServer, {
+  context: getContext,
+}));
+
+
+const httpServer = createHttpServer(app);
+const wsServer = new WebSocketServer({ server: httpServer, path: '/graphql'});
+useWsServer({ schema}, wsServer);
+
+httpServer.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+});
+
+
+// WebSocekt connection is started making a special http request
